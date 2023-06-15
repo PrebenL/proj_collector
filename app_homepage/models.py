2e021485961b82d51096a1a7c3713842bd523090
Preben <p21l07@hotmail.com>
2023-06-15 17:51:52 +0200

    fissue

--- app_homepage/models.py
+++ app_homepage/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 class Card(models.Model):
 	card_category_choices = [('dev', 'Development'), ('torrents', 'Torrents'),('media', 'Media')]
+	card_icon_choices = [('code', 'Development'), ('download', 'Torrents'),('film', 'Media')]
 	card_title = models.CharField(max_length=200)
 	card_category = models.CharField(max_length=100, choices=card_category_choices)
 	card_url = models.CharField(max_length=200)
+	card_icon = models.CharField(max_length=100, choices=card_icon_choices)
 	def __str__(self):
 		return self.card_title
 
--- app_homepage/views.py
+++ app_homepage/views.py
@@ -3,14 +3,14 @@ import app_homepage.utils.extraction as ext
 
 from .models import Card, Watch
 watch_headers = ["Show", "Episode", "Watch", "Download"]
+card_h = ['DEV','TORRENTS','MEDIA']
 def serve_homepage(request):
 	card = Card.objects.all()
 	watch = Watch.objects.all()
 	show_list = []
-
 	for x in watch:
 		show_list.append(x.show_name)
-
+		
 	wanted = ext.extractMostRecentAnimeList('https://gogoanime.llc/?page=', show_list)
-	context = {'card': card, 'watch': watch, 'show': wanted, 'w_header': watch_headers}
+	context = {'card': card, 'watch': watch, 'show': wanted, 'w_header': watch_headers, 'cardh': card_h}
 	return render(request, 'app_homepage/homepage_index.html', context)
