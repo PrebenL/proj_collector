dcb0126d098f3d4223e27e0200e795eedf113ada
Preben <p21l07@hotmail.com>
2023-06-21 14:42:14 +0200

    fixed urls

--- proj_collector/urls.py
+++ proj_collector/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     #path('series/', include("app_series.urls")),
-    path('home/', include("app_homepage.urls")),
+    path('', include("app_homepage.urls")),
     path('manga/', include("app_manga.urls")),
     path('anime/', include("app_anime.urls")),
     path('admin/', admin.site.urls),
